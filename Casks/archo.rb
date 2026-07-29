2172850aabb48099fa6ae07df4831a05db68cd6c
Onur Şahin <31903339+imonursahin@users.noreply.github.com>
2026-07-29 08:45:29 +0000

    archo 0.1.12

--- Casks/archo.rb
+++ Casks/archo.rb
@@ -1,6 +1,6 @@
 cask "archo" do
-  version "0.1.11"
-  sha256 "6c5a80f0abb72066d38d89e4a726f76908b35aa84c3c1b2494f61cd902fe8b50"
+  version "0.1.12"
+  sha256 "d45b69827f0e4c37f0bbb0af805f4eed98fad9b75acee6544402e4c87fabeb18"
 
   url "https://github.com/imonursahin/archo/releases/download/v#{version}/Archo-#{version}-arm64.dmg"
   name "Archo"
